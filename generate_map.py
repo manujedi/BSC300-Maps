1d33587555291d8bb7632071fd17080beb0607e9
manu <a@b.c>
2025-12-31 13:59:02 +0100

    speed + fix naming

--- generate_map.py
+++ generate_map.py
@@ -14,13 +14,29 @@ from datetime import datetime
 # config
 bin_dir = "osmosis/bin"
 tag_file = "tag-igpsport.xml"
-#cmd = './osmosis --rbf file={input_map_file} --mapfile-writer file={output_map_file} type=hd zoom-interval-conf=13,13,13,14,14,14 threads=4 tag-conf-file={tag_file}'
-cmd = './osmosis --rbf file={input_map_file} --tag-filter reject-ways amenity=* highway=* building=* natural=* landuse=* leisure=* shop=* waterway=* man_made=* railway=* tourism=* barrier=* boundary=* power=* historic=* emergency=* office=* craft=* healthcare=* aeroway=* route=* public_transport=* bridge=* tunnel=* addr:housenumber=* addr:street=* addr:city=* addr:postcode=* name=* ref=* surface=* access=* foot=* bicycle=* motor_vehicle=* oneway=* lit=* width=* maxspeed=* mountain_pass=* religion=* tracktype=* area=* sport=* piste=* admin_level=* aerialway=* lock=* roof=* military=* wood=* --tag-filter accept-relations natural=water place=islet --used-node --rbf file={input_map_file} --tag-filter accept-ways highway=* waterway=* landuse=* natural=* place=* --tag-filter accept-relations highway=* waterway=* landuse=* natural=* place=* --used-node --merge --mapfile-writer file={output_map_file} type=hd zoom-interval-conf=13,13,13,14,14,14 threads=4 tag-conf-file={tag_file}'
+# cmd = './osmosis --rbf file={input_map_file} --mapfile-writer file={output_map_file} type=hd zoom-interval-conf=13,13,13,14,14,14 threads=4 tag-conf-file={tag_file}'
+
+# cmd = ('./osmosis --rbf file={input_map_file} '
+#        '--tag-filter reject-ways amenity=* highway=* building=* natural=* landuse=* leisure=* shop=* waterway=* man_made=* railway=* tourism=* barrier=* boundary=* power=* historic=* emergency=* office=* craft=* healthcare=* aeroway=* route=* public_transport=* bridge=* tunnel=* addr:housenumber=* addr:street=* addr:city=* addr:postcode=* name=* ref=* surface=* access=* foot=* bicycle=* motor_vehicle=* oneway=* lit=* width=* maxspeed=* mountain_pass=* religion=* tracktype=* area=* sport=* piste=* admin_level=* aerialway=* lock=* roof=* military=* wood=* --tag-filter accept-relations natural=water place=islet '
+#        '--used-node '
+#        '--rbf file={input_map_file} --tag-filter accept-ways highway=* waterway=* landuse=* natural=* place=* '
+#        '--tag-filter accept-relations highway=* waterway=* landuse=* natural=* place=* '
+#        '--used-node '
+#        '--merge '
+#        '--mapfile-writer file={output_map_file} type=hd zoom-interval-conf=13,13,13,14,14,14 threads=4 tag-conf-file={tag_file}')
+
+cmd = (
+    './osmosis --rbf file={input_map_file} workers=4 '
+    '--tag-filter accept-ways highway=* waterway=* landuse=* natural=* place=* '
+    '--tag-filter accept-relations highway=* waterway=* landuse=* natural=* place=* '
+    '--used-node '
+    '--mapfile-writer file={output_map_file} type=hd zoom-interval-conf=13,13,13,14,14,14 threads=4 tag-conf-file={tag_file}'
+)
 
 if not os.path.isdir(bin_dir):
     print("setup osmosis first")
     exit(1)
-    
+
 # parser
 parser = argparse.ArgumentParser(
     description="Usage: -i <input_map_file> -c <country> -s <state>"
@@ -45,14 +61,17 @@ tag_file = os.path.realpath(tag_file)
 tmp_map_file = os.path.realpath("tmp.map")
 bin_dir = os.path.realpath(bin_dir)
 
-#make sure tmp is not in ram
+# make sure tmp is not in ram
 temp_dir = Path("tmp").mkdir(parents=True, exist_ok=True)
 os.environ["JAVA_TOOL_OPTIONS"] = "-Djava.io.tmpdir=" + os.path.realpath("tmp")
 os.environ["_JAVA_OPTIONS"] = "-Xmx8g"
 
 # run
-cmd = cmd.format(input_map_file=input_map_file, output_map_file=tmp_map_file, tag_file=tag_file)
-process = subprocess.run(cmd.split(' '), cwd=bin_dir)
+cmd = cmd.format(
+    input_map_file=input_map_file, output_map_file=tmp_map_file, tag_file=tag_file
+)
+print("running" + cmd)
+process = subprocess.run(cmd.split(" "), cwd=bin_dir)
 
 if process.returncode != 0:
     exit(process.returncode)
@@ -73,12 +92,13 @@ class MapsForgeHeader:
     mapStartLat: int
     mapStartLon: int
     start_zoom_level: int
-    language:str
-    comment:str
-    created_by:str
-    poiTags:list
-    wayTags:list
-    zoom_interval_configs:list
+    language: str
+    comment: str
+    created_by: str
+    poiTags: list
+    wayTags: list
+    zoom_interval_configs: list
+
 
 def parseMapsForgeHeader(file: Path) -> MapsForgeHeader:
 
@@ -104,7 +124,7 @@ def parseMapsForgeHeader(file: Path) -> MapsForgeHeader:
         idx += 1
 
         return value, idx
-    
+
     data = open(file, "rb").read(2000)
     header = MapsForgeHeader()
     idx = 0
@@ -112,7 +132,6 @@ def parseMapsForgeHeader(file: Path) -> MapsForgeHeader:
     idx += 20
     header.header_size = int.from_bytes(data[idx : idx + 4])
     idx += 4
-
 
     assert header.header_size < 1900, "didn't read enough bytes"
 
@@ -197,7 +216,7 @@ def parseMapsForgeHeader(file: Path) -> MapsForgeHeader:
         header.wayTags.append(tag)
     header.wayTags.sort()
 
-    amount_zoom_intervals = int.from_bytes(data[idx:idx+1])
+    amount_zoom_intervals = int.from_bytes(data[idx : idx + 1])
     idx += 1
 
     header.zoom_interval_configs = []
@@ -212,25 +231,31 @@ def parseMapsForgeHeader(file: Path) -> MapsForgeHeader:
         idx += 8
         sizesub = int.from_bytes(data[idx : idx + 8])
         idx += 8
-        header.zoom_interval_configs.append({
-            "basezoom": basezoom,
-            "minzoom": minzoom,
-            "maxzoom": maxzoom,
-            "absstart": absstart,
-            "sizesub": sizesub,
-        })
+        header.zoom_interval_configs.append(
+            {
+                "basezoom": basezoom,
+                "minzoom": minzoom,
+                "maxzoom": maxzoom,
+                "absstart": absstart,
+                "sizesub": sizesub,
+            }
+        )
 
     # assert(idx - 24 == header.header_size)
 
     return header
 
+
 # with leading zeros, 3digits
 def intToB36(number):
-    return '0' * (3 - len(np.base_repr(number, base=36))) + np.base_repr(number, base=36)
+    return "0" * (3 - len(np.base_repr(number, base=36))) + np.base_repr(
+        number, base=36
+    )
+
 
 def merY(lat):
     lat_rad = (lat * math.pi) / 180
-    return (1 - math.log(math.tan(lat_rad) + (1/math.cos(lat_rad))) / math.pi) / 2
+    return (1 - math.log(math.tan(lat_rad) + (1 / math.cos(lat_rad))) / math.pi) / 2
 
 
 header = parseMapsForgeHeader(Path(tmp_map_file))
@@ -238,21 +263,23 @@ pprint.pp("generated file:")
 pprint.pp(vars(header))
 
 # some checks if file was correctly created
-assert b'mapsforge binary OSM' == header.magic_byte
+assert b"mapsforge binary OSM" == header.magic_byte
 assert os.path.getsize(tmp_map_file) == header.file_size
 
 
-n = (1<<13)
-lon_start = math.floor(((header.minLon/1000000 + 180) / 360) * n)
-lat_start = math.floor(merY(header.maxLat/1000000) * n)
-lon_end = math.floor(((header.maxLon/1000000 + 180) / 360) * n)
-lat_end = math.floor(merY(header.minLat/1000000) * n)
+n = 1 << 13
+lon_start = math.floor(((header.minLon + 180) / 360) * n)
+lat_start = math.floor(merY(header.maxLat) * n)
+lon_end = math.floor(((header.maxLon + 180) / 360) * n)
+lat_end = math.floor(merY(header.minLat) * n)
 lon_diff = abs(lon_start - lon_end)
 lat_diff = abs(lat_start - lat_end)
 
 prefix = args.c + args.s
-date = datetime.today().strftime('%y%m%d')
-postfix = intToB36(lon_start) + intToB36(lat_start) + intToB36(lon_diff) + intToB36(lat_diff)
+date = datetime.today().strftime("%y%m%d")
+postfix = (
+    intToB36(lon_start) + intToB36(lat_start) + intToB36(lon_diff) + intToB36(lat_diff)
+)
 
 out_map_file = os.path.realpath(prefix + date + postfix + ".map")
 
