53f0d6a774c7378dba6efda1ebc145377f4ffb3f
Jedinger, Manuel <manuel.jedinger@student.tugraz.at>
2026-07-17 09:35:18 +0200

    increase heap, not sure if github containers support this

--- generate_map.py
+++ generate_map.py
@@ -68,7 +68,7 @@ bin_dir = os.path.realpath(bin_dir)
 temp_dir = str(int(datetime.today().timestamp()*1000)) + "_tmp"
 Path(temp_dir).mkdir(parents=True, exist_ok=True)
 os.environ["JAVA_TOOL_OPTIONS"] = "-Djava.io.tmpdir=" + os.path.realpath(temp_dir)
-os.environ["_JAVA_OPTIONS"] = "-Xmx8g"
+os.environ["_JAVA_OPTIONS"] = "-Xmx16g"
 
 # run
 cmd = cmd.format(
